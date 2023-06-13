12214e6d15480bdf153e936de7f6709a4eea4dcc
KonradSadowski <darnokowsky@gmail.com>
2023-06-13 20:02:42 +0200

    ustawione

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feedback extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return <div>Class Component</div>;
+  }
+}
+
+export default Feedback;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import Feedback from 'components/Feedback/Feedback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Feedback />
   </React.StrictMode>
 );
